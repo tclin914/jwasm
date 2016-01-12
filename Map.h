e797111bf754bb5272c4111a3a5f6870ce038908
tclin <tclin914@gmail.com>
2016-01-12 21:47:40 +0800

    add Map.h for data store

---
+++ Map.h
@@ -0,0 +1,17 @@
+#include <map>
+#include "UTF8.h"
+
+namespace jwasm {
+
+    class CommonJavaClass;
+
+    class ClassMap {
+        public: 
+            ClassMap() {}
+            ClassMap(std::map<const jwasm::UTF8*, CommonJavaClass*>* classmap) : map(*classmap) {}
+
+            std::map<const jwasm::UTF8*, CommonJavaClass*> map;
+            typedef std::map<const jwasm::UTF8*, CommonJavaClass*>::iterator iterator;
+    };
+
+}
